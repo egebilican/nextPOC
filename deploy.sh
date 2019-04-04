659cc14e02a5031165069225ff928a63fb0e3ba6
Ege <ege.bilican@adesso.com.tr>
2019-04-04 11:10:50 +0300

    deneme

--- deploy.sh
+++ deploy.sh
@@ -115,9 +115,9 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
   echo "Running $NPM_CMD install --production"
   echo "Hadi basladik bakalim"
-  eval $NPM_CMD install --production
+  eval $NPM_CMD install
   echo "bi daha"
-  eval $NPM_CMD install --production
+  eval $NPM_CMD install
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
